29efd626d8d19b30460b01b2708a7270d2895a6e
wdyjason <ddd123321456555@qq.com>
2020-07-24 15:10:27 +0800

    finish 2 exercise

--- src/main/java/com/twc/javaBasic/MyStack.java
+++ src/main/java/com/twc/javaBasic/MyStack.java
@@ -24,7 +24,8 @@ public class MyStack {
 
         // TODO: Please push the value into the storage here.
         // <--start
-
+        storage[count] = value;
+        count++;
         // --end-->
     }
 
@@ -37,7 +38,12 @@ public class MyStack {
         //  IMPORTANT: You SHOULD NOT USE COLLECTIONS (such as List<T>) OTHER THAN ARRAY.
         //
         // <--start
-
+        int[] swapStorage = storage.clone();
+        storage = new int[newCapacity];
+        for (int i = 0; i < capacity; i ++) {
+            storage[i] = swapStorage[i];
+        }
+        capacity = newCapacity;
         // --end-->
     }
 
@@ -57,7 +63,10 @@ public class MyStack {
         //  Please pop one element from the array. You should update the `count` field
         //  as well.
         // <--start
-
+        if (count != 0) {
+            count --;
+            return storage[count];
+        }
         // --end-->
 
         throw new UnsupportedOperationException("Stack is empty.");
